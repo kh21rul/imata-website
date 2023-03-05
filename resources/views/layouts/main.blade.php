1c62d67417f7fbe28be11a733f72cda0be2638c0
Khairul Aqram <khairulaqram21@gmail.com>
2023-03-05 21:21:40 +0700

    add google site verification

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta content="width=device-width, initial-scale=1.0" name="viewport"> 
+  <meta name="google-site-verification" content="BGqcRIEgJloemN_wB6OZz6W64kkIvtbE2RZWJP-B-Co" />
   <meta name="keywords" content="{{ $meta->keywords ?? '' }}">
   <meta name="author" content="{{ $meta->author ?? '' }}">
   <meta name="description" content="{{ $meta->description ?? '' }}">
