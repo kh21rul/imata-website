6d369f68d2f224398e992c932c838a1f72efeb9b
Khairul Aqram <khairulaqram21@gmail.com>
2024-05-03 00:07:13 +0700

    Kembalikan task scheduler menjadi seminggu sekali

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,8 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        // $schedule->command('sitemap:generate')->weekly();
-        $schedule->command('sitemap:generate')->everyMinute();
+        $schedule->command('sitemap:generate')->weekly();
     }
 
     /**
