71949860a768e448fa91e607cc89f6da6b6b489a
Khairul Aqram <khairulaqram21@gmail.com>
2024-06-15 18:19:41 +0700

    fix: perbaiki meta url divisi

--- app/Http/Controllers/DivisiController.php
+++ app/Http/Controllers/DivisiController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Divisi;
 use stdClass;
 
@@ -23,32 +22,32 @@ class DivisiController extends Controller
 
     public function show($divisi)
     {
-        $nama_divisi = $divisi;
-        if ($nama_divisi == 'teras') {
+        $url_divisi = $divisi;
+        if ($url_divisi == 'teras') {
             $nama_divisi = 'Pengurus Teras';
-        } else if ($nama_divisi == 'koordinator') {
+        } else if ($url_divisi == 'koordinator') {
             $nama_divisi = 'Koordinator Wilayah';
-        } else if ($nama_divisi == 'agama') {
+        } else if ($url_divisi == 'agama') {
             $nama_divisi = 'Divisi Keagamaan';
-        } else if ($nama_divisi == 'kaderisasi') {
+        } else if ($url_divisi == 'kaderisasi') {
             $nama_divisi = 'Divisi Kaderisasi';
-        } else if ($nama_divisi == 'sekretariat') {
+        } else if ($url_divisi == 'sekretariat') {
             $nama_divisi = 'Divisi Kesekretariatan';
-        } else if ($nama_divisi == 'infokom') {
+        } else if ($url_divisi == 'infokom') {
             $nama_divisi = 'Divisi Informasi dan Komunikasi';
-        } else if ($nama_divisi == 'humas') {
+        } else if ($url_divisi == 'humas') {
             $nama_divisi = 'Divisi Hubungan Masyarakat';
-        } else if ($nama_divisi == 'kwh') {
+        } else if ($url_divisi == 'kwh') {
             $nama_divisi = 'Divisi Kewirausahaan';
-        } else if ($nama_divisi == 'seni') {
+        } else if ($url_divisi == 'seni') {
             $nama_divisi = 'Divisi Seni dan Budaya';
-        } else {
+        } else if ($url_divisi == 'olahraga') {
             $nama_divisi = 'Pemuda dan Olahraga';
         }
 
         $this->meta->keywords = 'divisi ' . $nama_divisi . ' imata lhokseumawe - aceh utara, divisi imata lhokseumawe - aceh utara, imata lhokseumawe - aceh utara, imata, tamiang, mahasiswa tamiang';
         $this->meta->description = 'Divisi ' . $nama_divisi . ' Ikatan Mahasiswa Aceh Tamiang, Lhokseumawe - Aceh Utara.';
-        $this->meta->url = 'https://imata.web.id/divisi/' . $nama_divisi;
+        $this->meta->url = 'https://imata.web.id/divisi/' . $url_divisi;
 
         return view('divisi', [
             'title' => $nama_divisi,
